27b5ed54270202469b39d8b4962b4863ad2de79c
Jdilsham <janithadilsham@gmail.com>
2025-05-28 23:54:20 +0530

    rename

---
+++ API _02_dockerignore/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json([{
+    'Name' : 'Janitha Dilsham',
+    'Email' : 'janitha1717@gmail.com'
+},
+{
+    'Name' : 'Jani',
+    'Email' : 'jani@gmail.com'
+}
+
+])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ API _03_layer_and_caching/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+  res.json([{
+    'Name' : 'Janitha Dilsham',
+    'Email' : 'janitha1717@gmail.com'
+}])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
